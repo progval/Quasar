d9577270bee262c4b986e3f4f12aa452252277ae
Florent Revest <florent.revest666@gmail.com>
2011-07-12 16:56:18 +0200

    We now use Grub2 instead of legacy, and have a basic VESA support and a little reboot routine

--- boot/stage2/stage2.c
+++ boot/stage2/stage2.c
@@ -18,7 +18,7 @@
  */
 
 /*
- * loading part of stage2
+ * Loading part of stage2
  * The stage2's job is to get multiboot info, load and jump to the 
  * kernel module.
  */
@@ -27,6 +27,8 @@
 
 /* Macro that tells if the bit 'n' of 'flags' is set to 1 */
 #define IS_SET(flags,n) ((flags >> n) & 0x1)
+
+#define reboot() outb(0x64, 0xFE)
 
 /*
  * Entry point, called from the ASM file. Main method of stage2
@@ -38,7 +40,7 @@ int main(unsigned long magic, struct multiboot *mboot)
     unsigned int bootdev, flags;
     int i = 0;
     
-    /* clear screen */
+    /* Clear screen */
     for(i; i < 160*25; i++)
         *(video + i) = 0;
         
@@ -48,38 +50,31 @@ int main(unsigned long magic, struct multiboot *mboot)
         printf("> Booting from a multiboot-compliant bootloader... \n");
         flags = mboot->flags;
         
-        /* read mem info : bit 0 indicates if mem fields are valids */
-        if(IS_SET(flags,0))
+        /* Read mem info : bit 0 indicates if mem fields are valids */
+        if(IS_SET(flags, 0))
         {
             printf("> Lower memory : found %d Kb \n", mboot->mem_lower);
             printf("> Upper memory : found %d Kb \n", mboot->mem_upper);
         } else printf("> Unable to get memory info \n");
         
-        /* where are we booting from ? 
-         * bit 1 indicates if this field is ok */
-        if(IS_SET(flags,1))
+        /* Where are we booting from ? 
+         * Bit 1 indicates if this field is ok */
+        if(IS_SET(flags, 1))
         {
             bootdev = mboot->boot_device;
             printf("> Booting from BIOS device %d\n", (bootdev & 0xFF));
         } 
         
         /* Read the kernel command line */
-        if(IS_SET(flags,2))
+        if(IS_SET(flags, 2))
         {
             cmdline = (char *) mboot->cmd_line;
             printf("> Kernel command line : %s \n", cmdline);
         }
-        
-        /* check if we have VESA */
-        if(IS_SET(flags,11))
-            printf("GNAGNAGNA VESA \n");
-        else
-            printf("pas gnagnagna :'( \n");
     }
     else
-        printf("> Booting from a unknown bootloader \n");
+        printf("> Booting from an unknown bootloader \n");
     
     whereami();
-    
     return 0;
 }
--- boot/stage2/update_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash	
-
-sudo /sbin/losetup /dev/loop0 floppy.img
-sudo mount /dev/loop0 /mnt
-sudo cp stage2.bin /mnt/system/stage2.bin
-sudo umount /dev/loop0
-sudo /sbin/losetup -d /dev/loop0
